7bf1e26dda3143ce61ae31b6d3bb5bee34d83357
Tibor Szabo <tibor_szabo@epam.com>
2021-04-22 09:43:27 +0200

    added input for new todo items

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 import { TodoItem } from 'todo.js';
 
+document.addEventListener('DOMContentLoaded', function(){
+    const todoList = [];
 
-const todoList = [];
+    const toggleTodoInput = () => {
+        
+    }
+
+    document.querySelector('#addTodo').addEventListener('click', toggleTodoInput);
+
+});
