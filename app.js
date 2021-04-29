1ad641cc8c8a5585be487471ee3080a2e42284c0
Tibor Szabo <tibor_szabo@epam.com>
2021-04-29 09:00:39 +0200

    implemented drag and drop with orders

--- app.js
+++ app.js
@@ -17,24 +17,34 @@ const todoInputVisible = visible => {
     }
 }
 
-const saveTodo = title => {
-    const newTodo = new TodoItem(title.trim());
+const addTodo = newTodo => {
+    if (!newTodo.order) {
+        newTodo.order = getHighestOrder(todoContainer) + 1;
+    }
+    todoList.forEach(todo => {
+        if (todo.order >= newTodo.order) {
+            todo.order++;
+        }
+    });
     todoList.push(newTodo);
     todoInputVisible(false);
     displayTodo(newTodo);
+    orderTodosView();
     saveToLocalStorage();
 }
 
 const displayTodo = todoItem => {
     const container = todoItem.done ? todoDoneContainer : todoContainer;
-    container.append(todoItem.done ? getTodoDoneMarkup(todoItem.title) : getTodoMarkup(todoItem.title));
+    container.append(todoItem.done ? getTodoDoneMarkup(todoItem) : getTodoMarkup(todoItem));
 }
 
-const getTodoMarkup = title => {
+const getTodoMarkup = todoItem => {
     const div = document.createElement('div');
     div.classList.add('todo-item');
+    div.setAttribute('draggable', 'true');
+    div.setAttribute('ondragstart', 'onTodoDragStart(event)');
     div.innerHTML =
-    `<div class="todo-item-title">${title}</div>
+    `<div class="todo-item-title">${todoItem.title}</div>
     <div class=" todo-item-icons">
         <i class="fas fa-pen" id="editTodo"></i>
         <i class="fas fa-trash-alt" id="deleteTodo"></i>
@@ -44,11 +54,13 @@ const getTodoMarkup = title => {
     return div;
 };
 
-const getTodoDoneMarkup = title => {
+const getTodoDoneMarkup = todoItem => {
     const div = document.createElement('div');
+    div.setAttribute('draggable', 'true');
+    div.setAttribute('ondragstart', 'onTodoDragStart(event)');
     div.classList.add('todo-item');
     div.innerHTML =
-    `<div class="todo-item-title">${title}</div>
+    `<div class="todo-item-title">${todoItem.title}</div>
     <div class=" todo-item-icons">
         <i class="fas fa-trash-alt" id="deleteTodo"></i>
         <i class="fas fa-clipboard" id="undoneTodo"></i>
@@ -102,20 +114,31 @@ const saveToLocalStorage = () => window.localStorage.setItem('todoList', JSON.st
 const initFromStorage = () => {
     const storedData = window.localStorage.getItem('todoList');
     if (storedData) {
-        todoList = JSON.parse(storedData).map(item => new TodoItem(item._title, item.done));
+        todoList = JSON.parse(storedData).map(item => new TodoItem(item._title, item.done, item._order));
         todoList.forEach(todoItem => displayTodo(todoItem));
+        orderTodosView();
     }
 }
 
-const deleteTodo = title => {
-    todoList = todoList.filter(item => item.title !== title);
+const deleteTodo = todoItem => {
+    todoList = todoList.filter(item => {
+        if (todoItem.order && item.order > todoItem.order) {
+            item.order -= 1;
+        }
+        return item.title !== todoItem.title;
+    });
     saveToLocalStorage();
 }
 
-const doneTodo = title => {
-    const todo = todoList.find(item => item.title === title);
-    todo.toggleStatus();
-    displayTodo(todo);
+const doneTodo = todoItem => {
+    todoItem.toggleStatus();
+    todoList.forEach(todo => {
+        if (todo.order > todoItem.order) {
+            todo.order--;
+        }
+    });
+    todoItem.order = null;
+    displayTodo(todoItem);
     saveToLocalStorage();
 }
 
@@ -149,23 +172,102 @@ const addEditInputEventListener = (inputEl, previousTitle) => {
 } 
 
 function addTodoItemEventListeners(todoItemElement) {
-    const todoItemTitle = todoItemElement.childNodes[0].innerHTML;
+    const todoItem = getTodoFromTitle(todoItemElement.childNodes[0].innerHTML);
     todoItemElement.addEventListener('click', function(event) {
         if (event.target.id === 'editTodo') {
             switchTitleWithInput(this);
         } else if (event.target.id === 'deleteTodo') {
             this.remove();
-            deleteTodo(todoItemTitle);
+            deleteTodo(todoItem);
         } else if (event.target.id === 'doneTodo') {
             this.remove();
-            doneTodo(todoItemTitle);
+            doneTodo(todoItem);
         } else if (event.target.id === 'undoneTodo') {
-            this.remove();
-            deleteTodo(todoItemTitle);
-            saveTodo(todoItemTitle);
+            undoTodoItem(todoItem);
         }
     })
 }
+
+const undoTodoItem = todoItem => {
+    removeTodoFromDOM(todoItem);
+    deleteTodo(todoItem);
+    addTodo(new TodoItem(todoItem.title));
+    orderTodosView();
+}
+
+const allowDrop = event => event.preventDefault();
+
+const onTodoDragStart = event => {
+    const todoTitle = event.srcElement.firstChild.innerHTML;
+    event.dataTransfer.setData('todoTitle', todoTitle);
+}
+
+const onTodoDrop = (event, droppedToTodoDone) => {
+    event.preventDefault();
+    const droppedTodo = getTodoFromTitle(event.dataTransfer.getData('todoTitle'));
+    if (droppedToTodoDone && droppedTodo.done) {
+        removeTodoFromDOM(droppedTodo);
+        deleteTodo(droppedTodo);
+        addTodo(droppedTodo);
+    } else if (!droppedToTodoDone && !droppedTodo.done) {
+        removeTodoFromDOM(droppedTodo);
+        doneTodo(droppedTodo);
+    } else if (droppedToTodoDone && !droppedTodo.done && droppedOnTodoItem(event) && !droppedOnSelf(event, droppedTodo)) {
+        const targetTodoTitle = event.path.find(el => el.className === 'todo-item').firstChild.innerHTML;
+        const targetTodoOrder = getTodoFromTitle(targetTodoTitle).order;
+        const newOrder = droppedUnderElement(event) ? targetTodoOrder + 1 : targetTodoOrder;
+        removeTodoFromDOM(droppedTodo);
+        deleteTodo(droppedTodo);
+        addTodo(new TodoItem(droppedTodo.title, false, newOrder))
+    }
+}
+
+const droppedOnSelf = (dragEvent, todoItem) => 
+    dragEvent.path.find(el => el.className === 'todo-item').firstChild.innerHTML === todoItem.title;
+
+const droppedUnderElement = dragEvent => {
+    const droppedOn = dragEvent.path.find(el => el.className === 'todo-item');
+    var rect = droppedOn.getBoundingClientRect();
+    return dragEvent.clientY > (rect.top + rect.bottom) / 2;
+}
+
+const droppedOnTodoItem = dragEvent => {
+    for (let element of dragEvent.path) {
+        if (element.className === 'todo-item') {
+            return true;
+        }
+    }
+    return false;
+}
+
+const removeTodoFromDOM = (todoItem) => {
+    let found = null;
+    const removeFromContainer = todoItem.done ? todoDoneContainer : todoContainer;
+    for (let todo of removeFromContainer.children) {
+        if (todo.firstChild.innerHTML === todoItem.title) {
+            found = todo;
+            break;
+        }
+    }
+    found.remove();
+}
+                                    
+const getHighestOrder = () => todoContainer.childNodes.length;
+
+const changeTodoOrder = (todoTitle, newOrder) => {
+    todoList = todoList.map(todo => {
+
+    });
+}
+
+const orderTodosView = () => {
+    for (let todo of todoContainer.children) {
+        const order = getTodoFromTitle(todo.firstChild.innerHTML).order;
+        todo.style.order = order;
+    }
+}
+
+const getTodoFromTitle = title => todoList.find(i => i.title === title);
 
 document.addEventListener('DOMContentLoaded', function(){
 
@@ -173,12 +275,14 @@ document.addEventListener('DOMContentLoaded', function(){
 
     addTodoButton.addEventListener('click', () => todoInputVisible(true));
 
+    document.querySelector('#test').addEventListener('click', () => console.log(todoList));
+
     todoInput.addEventListener('keyup', event => {
         if (event.code === 'Escape') {
             todoInputVisible(false);
         } else if (event.code === 'Enter' || event.code === 'NumpadEnter') {
             if (validateTodo(todoInput.value)) {
-                saveTodo(todoInput.value);
+                addTodo(new TodoItem(todoInput.value));
             }
         }
     });
--- todo.js
+++ todo.js
@@ -1,13 +1,18 @@
 class TodoItem {
 
-    constructor(title, done = false) {
+    constructor(title, done = false, order = null) {
         this.title = title;
         this.done = done;
+        this.order = order;
     }
 
     get title() { return this._title; }
 
     set title(newTitle) { this._title = newTitle; }
+
+    get order() { return this._order; }
+
+    set order(newORder) { this._order = newORder; }
 
     toggleStatus() { this.done = !this.done; }
 
